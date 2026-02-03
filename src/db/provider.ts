7871f1efe22bed0ed2e4788c0d448ca2dce51938
Nikita Kogut <glinka1202@gmail.com>
2026-02-03 21:55:07 +0200

    Add pooling to DB connection, close #26

--- src/db/provider.ts
+++ src/db/provider.ts
@@ -1,11 +1,17 @@
 import { AppConfig } from '../config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
+import { Pool } from 'pg';
+
 export class DBProvider {
   private db;
+  private pool: Pool;
 
   constructor(private readonly config: AppConfig) {
-    this.db = drizzle(config.dbUrl);
+    this.pool = new Pool({
+      connectionString: config.dbUrl,
+    });
+    this.db = drizzle(this.pool);
   }
 
   public get() {
